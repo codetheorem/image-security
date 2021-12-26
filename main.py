354a48d1e7470276fc7bff8166656982468ef95e
Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>
2021-12-27 03:05:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+from fastapi import FastAPI, File, UploadFile
+
+from fastapi.middleware.cors import CORSMiddleware
+
+import shutil
+
+from fastapi.responses import FileResponse
+
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def encrypt(file):
+ fo = open(file, "rb")
+ image=fo.read()
+ fo.close()
+ image=bytearray(image)
+ key=random.randint(0,256)
+ for index,value in enumerate(image):
+  image[index] = value^key
+ fo=open("enc.jpg","wb")
+ imageRes="enc.jpg"
+ fo.write(image)
+ fo.close()
+ return (key,imageRes)
+
+def decrypt(key,file):
+ key=int(key)
+ fo = open(file, "rb")
+ image=fo.read()
+ fo.close()
+ image=bytearray(image)
+ for index , value in enumerate(image):
+  image[index] = value^key
+ fo=open("dec.jpg","wb")
+ imageRes="dec.jpg"
+ fo.write(image)
+ fo.close()
+ return imageRes
+
+
+@app.post("/files/")
+async def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
+
+
+import random
+import os
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/fileencrypt")
+async def fileencrypt(file: UploadFile = File(...)): 
+        with open(file.filename, "wb") as buffer:
+          shutil.copyfileobj(file.file, buffer)
+        key,image=encrypt(file.filename)
+        return {"name":file.filename,"key":key,"image":image}
+
+@app.post("/filedecrypt")
+async def filedecrypt(key,file: UploadFile = File(...)):
+        with open(file.filename, "wb") as buffer:
+          shutil.copyfileobj(file.file, buffer)
+        image=decrypt(key,file.filename)
+        return {"name":file.filename,"image":image}
+
+@app.get("/return-encfile")
+def return_file():
+    return FileResponse("./enc.jpg",filename="enc.jpg")
+
+@app.get("/return-decfile")
+def return_file():
+    return FileResponse("./dec.jpg",filename="dec.jpg")
